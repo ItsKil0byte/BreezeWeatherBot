eeaba2798c06df7e52edbddb4be2d1320829def8
Alexander <itskilobyte@gmail.com>
2024-08-31 18:50:08 +0500

    Добавлен хендлер для вывода текущей погоды.

--- bot/handlers/user_interaction.py
+++ bot/handlers/user_interaction.py
@@ -2,7 +2,8 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from messages import START
+from messages import START, WEATHER_ANSWER
+from utils import get_temp
 
 router = Router()
 router.message.filter(F.chat.type.in_(["private"]))
@@ -11,3 +12,23 @@ router.message.filter(F.chat.type.in_(["private"]))
 @router.message(Command("start"))
 async def command_start(message: Message):
     await message.answer(START)
+
+
+@router.message(F.text)
+async def send_weather_by_city(message: Message):
+    try:
+        data = get_temp(message.text)
+
+        await message.answer(
+            WEATHER_ANSWER.format(
+                city=data["city"],
+                time=data["time"],
+                weather=data["weather"],
+                temp=data["temp"],
+                temp_like=data["temp_like"],
+            )
+        )
+
+    except Exception:
+        # TODO усовершеснтвовать для более понятного ответа + логирование.
+        await message.answer("Упс. Кажется, что-то пошло не так.")
--- bot/utils.py
+++ bot/utils.py
@@ -1,5 +1,7 @@
 from aiogram import Bot
 from aiogram.types import BotCommand
+import requests
+from configurator import config
 
 
 async def set_commands(bot: Bot):
@@ -8,3 +10,22 @@ async def set_commands(bot: Bot):
     ]
 
     await bot.set_my_commands(commands)
+
+
+def get_temp(city: str):
+    key = config.api_key.get_secret_value()
+    url = f"http://api.weatherapi.com/v1/current.json?key={key}&q={city}&lang=ru"
+
+    response = requests.get(url=url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "city": data["location"]["name"],
+            "time": data["location"]["localtime"],
+            "weather": data["current"]["condition"]["text"],
+            "temp": data["current"]["temp_c"],
+            "temp_like": data["current"]["feelslike_c"],
+        }
+    else:
+        raise Exception
